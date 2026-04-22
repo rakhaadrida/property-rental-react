08bc9850f1ce0ea7cf919673d0d15b182e001a37
rakhaadrida <62930338+rakhaadrida@users.noreply.github.com>
2026-04-22 16:15:23 +0700

    hero component

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,16 @@
 import Navbar from "../components/navbar/Navbar";
+import Hero from "../sections/landing/Hero";
+import LandingPageSpecs from "../../specs/landingPage.json";
 
 const LandingPage = () => {
   return (
     <>
       <Navbar />
+      <Hero
+        travelers={LandingPageSpecs.hero.travelers}
+        treasures={LandingPageSpecs.hero.treasures}
+        cities={LandingPageSpecs.hero.cities}
+      />
     </>
   );
 };
